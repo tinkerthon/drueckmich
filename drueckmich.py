63f0d5aacb0a602c9b259a44ca1bf625fdc58263
Olav Schettler <olav@schettler.net>
2019-03-28 22:51:14 +0100

    For Pyxel: Complete

--- drueckmich.py
+++ drueckmich.py
@@ -11,13 +11,13 @@ RUNTER = 2
 LINKS = 3
 RECHTS = 4
 
+LAUFEND = 1
+AUSWERTUNG = 2
+
 class Seite:
     def __init__(self, wie_oft, zeilen):
         self.wie_oft = wie_oft
         self.zeilen = zeilen
-        self.auswahl_zeile = 0
-        self.auswahl_spalte = 0
-        self.auswahl = []
 
         self.anz_spalten = 0
         for z in range(0, len(zeilen)):
@@ -26,8 +26,13 @@ class Seite:
         
         self.spalten_breite = BREITE / self.anz_spalten
         self.zeilen_hoehe = HOEHE / len(self.zeilen)
-                
+        self.reset()
     
+    def reset(self):
+        self.auswahl_zeile = 0
+        self.auswahl_spalte = 0
+        self.auswahl = []        
+
     def zeige_auswahl(self, zeile, spalte, farbe):
         x1 = spalte * self.spalten_breite
         x2 = (spalte + 1) * self.spalten_breite - 1
@@ -36,7 +41,6 @@ class Seite:
         
         pyxel.rectb(x1, y1, x2, y2, farbe)
         
-
     def zeige(self):
         '''
         print("z=%d s=%d az=%d, as=%d" % (
@@ -58,8 +62,7 @@ class Seite:
                     
                 x = round(spalte * self.spalten_breite + self.spalten_breite / 3, 0)
                 y = round(zeile * self.zeilen_hoehe + self.zeilen_hoehe / 3, 0)
-                pyxel.text(x, y, text, pyxel.frame_count % 16)
-                
+                pyxel.text(x, y, text, pyxel.frame_count % 16)        
 
     def waehle(self, taste):
         if taste == RUNTER:
@@ -77,10 +80,90 @@ class Seite:
                 
     def merke(self):
         if not self.ist_ausgewaehlt():
+            
+            # Jede Zeile darf nur einmal ausgewählt werden
+            for auswahl in self.auswahl:
+                if self.auswahl_zeile == auswahl["zeile"]:
+                    return
+
             self.auswahl.append({"zeile": self.auswahl_zeile, "spalte": self.auswahl_spalte})
     
     def ist_ausgewaehlt(self):
         return len(self.auswahl) >= self.wie_oft
+
+    def als_zahl(self):
+        zahl = 0
+        
+        for auswahl in self.auswahl:
+            zahl += ((self.anz_spalten + 1) ** auswahl["zeile"]) * (auswahl["spalte"] + 1)
+        
+        return zahl
+
+
+class Auswertung:
+    def __init__(self, seiten):
+        texte = {
+            
+            # Zootier
+            1: {
+                # Dreieck
+                6: "einen Strauss",
+                7: "einen Flamingo",
+                8: "einen Papagei",
+                9: "eine Fledermaus",
+                
+                # Kreis
+                11: "einen Fuchs",
+                12: "einen Wolf",
+                13: "einen Ameisenbär",
+                14: "ein Erdmaennchen",
+                
+                # Quadrat
+                16: "einen Elefant",
+                17: "eine Giraffe",
+                18: "ein Nashorn",
+                19: "ein Nilpferd",
+                
+                # Rechteck
+                21: "ein Stachelschwein",
+                22: "eine Ziege",
+                23: "ein Huhn",
+                24: "einen Hahn"
+            },
+            # Haustier
+            2: {
+                # Dreieck
+                6: "ein Kaninchen",
+                7: "ein Meerschweinchen",
+                8: "einen Hamster",
+                9: "einen Fisch",
+                
+                # Kreis
+                11: "einen Frosch",
+                12: "ein Hausschwein",
+                13: "eine Garnele",
+                14: "ein Huhn",
+                
+                # Quadrat
+                16: "einen Hund",
+                17: "einen Katze",
+                18: "einen Papagei",
+                19: "einen Wellensittich",
+            
+                # Rechteck
+                21: "einen Hahn",
+                22: "eine Maus",
+                23: "eine Ratte",
+                24: "eine Kakerlake"
+            }
+        }
+        
+        self.seite = Seite(0, [
+            [ "Du brauchst\n" + texte[seiten[1].als_zahl()][seiten[3].als_zahl()] ]
+        ])
+    
+    def zeige(self):
+        return self.seite.zeige()
 
 
 class App:
@@ -89,6 +172,7 @@ class App:
         self.welche_seite = 0
         self.seiten = []
         self.seite = None
+        self.phase = LAUFEND
         
         self.seiten.append(Seite(1, [
             ["Drueckmich\nTiere"]
@@ -108,21 +192,31 @@ class App:
 
         pyxel.init(BREITE, HOEHE, caption="Drück mich")
         pyxel.run(self.update, self.draw)
-
-
+    
+    
     def update(self):
-        
-        self.seite = self.seiten[self.welche_seite]
+        if self.phase == AUSWERTUNG:
+            self.seite = Auswertung(self.seiten)
+        else:
+            self.seite = self.seiten[self.welche_seite]
 
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
         elif pyxel.btnp(pyxel.KEY_SPACE):
+            
+            if self.phase == AUSWERTUNG:
+                self.welche_seite = 0
+                self.phase = LAUFEND
+                for seite in self.seiten:
+                    seite.reset()
+                return
+            
             self.seite.merke()
             if self.seite.ist_ausgewaehlt():
                 if self.welche_seite < len(self.seiten) - 1:
                     self.welche_seite += 1
                 else:
-                    print("AUSWERTUNG")
+                    self.phase = AUSWERTUNG
         elif pyxel.btnp(pyxel.KEY_LEFT):
             self.seite.waehle(LINKS)
         elif pyxel.btnp(pyxel.KEY_RIGHT):
